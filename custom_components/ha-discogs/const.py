d880bdb8684fe65ef7279431a0cd503c0d01e127
Josh K <28068117+iamjoshk@users.noreply.github.com>
2025-08-25 17:15:30 -0400

    Update const.py

--- custom_components/ha-discogs/const.py
+++ custom_components/ha-discogs/const.py
@@ -1,2 +1,18 @@
+"""Constants for the Discogs integration."""
+
 DOMAIN = "ha-discogs"
 DEFAULT_NAME = "Discogs"
+
+# Sensor Types
+SENSOR_COLLECTION_TYPE = "collection"
+SENSOR_WANTLIST_TYPE = "wantlist"
+SENSOR_RANDOM_RECORD_TYPE = "random_record"
+SENSOR_COLLECTION_VALUE_MIN_TYPE = "collection_value_min"
+SENSOR_COLLECTION_VALUE_MEDIAN_TYPE = "collection_value_median"
+SENSOR_COLLECTION_VALUE_MAX_TYPE = "collection_value_max"
+
+# Attributes
+UNIT_RECORDS = "records"
+ICON_RECORD = "mdi:album"
+ICON_PLAYER = "mdi:record-player"
+ICON_CASH = "mdi:cash"
